ec8c887675b7eeb68e646b53d2687a6116569c0f
uknowxd <kitisaksrisuk@gmail.com>
2021-07-29 18:09:06 +0700

    Update main_btc.py
    เพิ่มการเช็คแบบหลายสกุลเงิน

--- main_btc.py
+++ main_btc.py
@@ -1,9 +1,18 @@
 import requests
-# Check ราคา Bitcoin Start
+
 response = requests.get('https://api.bitkub.com/api/market/ticker')
 data = response.json()
 
-chk_btc = data['THB_BTC']['last']
+# เช็คแค่สกุลเงินเดี๋ยว
+#chk_btc = data['THB_BTC']['last']
+#print ('ราคา 1 Bitcoin ตอนนี้', f"{chk_btc:,}" , 'บาท')
 
-print ('ราคา 1 Bitcoin ตอนนี้', f"{chk_btc:,}" , 'บาท')
-# Check ราคา Bitcoin END
+
+# เช็คแบบหลายสกุลเงิน
+name_coin = ['THB_BTC','THB_ETH','THB_DOGE']
+while True:
+    for n_coin in name_coin:
+        m_coin = data[n_coin]
+        last_price = m_coin['last']
+        print(n_coin, f"{last_price:,}" , 'บาท')
+    break
