b60ad6ab24f7961d32a56016027cd211835a42ba
uknowxd <kitisaksrisuk@gmail.com>
2021-07-29 17:46:52 +0700

    Delete check_price.py

--- check_price.py
+++
@@ -1,9 +0,0 @@
-import requests
-# Check ราคา Bitcoin Start
-response = requests.get('https://api.bitkub.com/api/market/ticker')
-data = response.json()
-
-chk_btc = data['THB_BTC']['last']
-
-print ('ราคา 1 Bitcoin ตอนนี้', f"{chk_btc:,}" , 'บาท')
-# Check ราคา Bitcoin END
